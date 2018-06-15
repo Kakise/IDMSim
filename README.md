4b1cc0da6cac5bfb80681ebc6309193214422b1f
kakise <mail@kakise.me>
2018-06-15 22:32:51 +0200

    Added dependencies

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IDMSim
 IDMSim is a graphical & interactive simulation of the "Intelligent Driver Model" made in C++.
+
+# Dependencies
+This project depends on SDL2. It can be installed on Ubuntu 16.04 using this command:
+
+```sudo apt-get install libsdl2-dev
+    
+```
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     Cars.emplace_back(Vehicle(0, 130 / 3.6, 0));
 
     std::cout << "[DEBUG] La vitesse de la première voiture est: " << Cars.at(0).v() << " mètres par secondes." << std::endl;
-    
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cout << "[ERREUR] Erreur d'initialisation de la SDL." << std::endl;
         return EXIT_FAILURE;
